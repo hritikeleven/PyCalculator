b93208ac849738366a271c3e1bf928d7c8baac05
Ansab Ijaz <ansab@users.noreply.github.com>
2015-04-10 20:54:52 -0700

    Updated PyCalculator

--- PyCalculator.py
+++ PyCalculator.py
@@ -1,22 +1,38 @@
-def add(*args):
+def add(numbers =0, *args):
 	result = 0
+	for num in numbers:
+		result += num
 	for num in args:
 		result += num
 	return result
 
-def mul(*args):
+def mul(numbers =0, *args):
 	result = 1
+	for num in numbers:
+		result *= num
 	for num in args:
 		result *= num
 	return result
 
-def div(*args):
+def div(numbers =0, *args):
 	result = 1
+	for num in numbers:
+		result = num / result
 	for num in args:
 		result = num / result
 	return result
 
+def sub(numbers =0, *args):
+	result = 0
+	for num in numbers:
+		result -= num
+	for num in args:
+		result -= num
+	return result
+
 if __name__ == '__main__':
-	print add(1, 2, 3)
-	print mul(1, 2, 3)
-	print div(1, 2, 3)
+	numbers = [1, 2, 3, 4]
+	print "Sum of: " + str(numbers) + " = " + str(add(numbers, 1, 2))
+	print "Product of: " + str(numbers) + " = " + str(mul(numbers))
+	print "Division of: " + str(numbers) + " = " + str(div(numbers))
+	print "Subtraction of: " + str(numbers) + " = " + str(sub(numbers))
